e46959662e89598a3dae4063aab624e7400c47f4
VictorWong123 <victorwong315@gmail.com>
2026-07-08 15:48:37 -0400

    changed / to @ in work

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WorkItem = ({ job }) => (
     <article className="text-item">
         <p className="item-meta">{job.date}</p>
-        <h3>{job.title} / {job.company}</h3>
+        <h3>{job.title} @ {job.company}</h3>
         <p>{job.description}</p>
         <p className="item-tags">{job.tech.join(' / ')}</p>
     </article>
